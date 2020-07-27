afcd414b60f388a582fc126e5204face10576a48
Patryk Palej <patrykpalej@gmail.com>
2020-07-27 14:28:31 +0200

    Add a function for calculating overall rating of an offer

--- data_acquisition/calculate_overall_rating.py
+++ data_acquisition/calculate_overall_rating.py
@@ -1,5 +1,24 @@
+from datetime import datetime
+
+
 def calculate_overall_rating(offer):
 
-    offer["overall"] = offer["mileage"] * offer["price"] / 1e8
+    # Power, mileage, price
+    overall = 1e10 * offer["power"] / (offer["mileage"] * offer["price"])
 
-    return offer
+    # Owner
+    if offer["owner"] == "Firma":
+        overall = overall * 0.7
+
+    # Fuel
+    if offer["fuel"] == "Benzyna+LPG":
+        overall = overall*1.1
+
+    # Year
+    age = datetime.now().year - offer["prod_year"]
+    overall = overall * (1 - 0.074*age + 0.0024*age**2)
+
+    # Distance
+    overall = overall * (1 - 0.003*offer["distance"])
+
+    return round(overall, 2)
