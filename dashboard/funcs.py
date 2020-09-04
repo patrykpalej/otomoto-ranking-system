2f8e0e7d3a96b679e2bdbdb6d18ab649cd81df65
Patryk Palej <patrykpalej@gmail.com>
2020-09-04 21:14:45 +0200

    Move input hints logic to a function

--- dashboard/funcs.py
+++ dashboard/funcs.py
@@ -2,8 +2,22 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
 
-def funcs_used_in_logic_section():
-    pass
+def input_hint(hint_text, style_dict, input_1, input_2, min_limit, max_limit,
+               error_message, unit):
+
+    if input_1 and input_2:
+        hint_text += input_1 + "{} - ".format(unit) + input_2 + unit
+
+        if int(input_1) < min_limit or int(input_2) > max_limit:
+            hint_text = "Wybierz z zakresu {}{} - {}{}"\
+                .format(str(int(min_limit)), unit, str(int(max_limit)), unit)
+            style_dict["color"] = "red"
+
+        if int(input_1) > int(input_2):
+            hint_text = error_message
+            style_dict["color"] = "red"
+
+    return hint_text
 
 
 def draw_plot(backend_input1):
--- dashboard/logic.py
+++ dashboard/logic.py
@@ -42,18 +42,10 @@ def backend(price_input_1, price_input_2, year_input_1, year_input_2,
     year_style_dict = {"width": "15%", "position": "absolute", "left": "20%",
                        "top": "10%", "height": "10%", "font-size": 18,
                        "font-family": "Cambria"}
+    error_message = "Minimalny rok produkcji jest większy od maksymalnego"
 
-    if year_input_1 and year_input_2:
-        year_output += year_input_1 + " - " + year_input_2
-
-        if int(year_input_1) < 2000 or int(year_input_2) > 2019:
-            year_output = "Wybierz z zakresu 2000 - 2019"
-            year_style_dict["color"] = "red"
-
-        if int(year_input_1) > int(year_input_2):
-            year_output = "Minimalny rok produkcji jest większy od " \
-                          "maksymalnego"
-            year_style_dict["color"] = "red"
+    year_output = input_hint(year_output, year_style_dict, year_input_1,
+                             year_input_2, 2000, 2019, error_message, "")
 
     # 3. Mileage
     mileage_output = "Przebieg: "
@@ -61,17 +53,11 @@ def backend(price_input_1, price_input_2, year_input_1, year_input_2,
                           "left": "35%",
                           "top": "10%", "height": "10%", "font-size": 18,
                           "font-family": "Cambria"}
+    error_message = "Minimalny przebieg jest większy od maksymalnego"
 
-    if mileage_input_1 and mileage_input_2:
-        mileage_output += mileage_input_1 + "km - " + mileage_input_2 + "km"
-
-        if int(mileage_input_1) < 1e4 or int(mileage_input_2) > 1e6:
-            mileage_output = "Wybierz z zakresu 10k - 1M"
-            mileage_style_dict["color"] = "red"
-
-        if int(mileage_input_1) > int(mileage_input_2):
-            mileage_output = "Minimalny przebieg jest większy od maksymalnego"
-            mileage_style_dict["color"] = "red"
+    mileage_output = input_hint(mileage_output, mileage_style_dict,
+                                mileage_input_1, mileage_input_2, 1e4, 1e6,
+                                error_message, "km")
 
     return [price_output, price_style_dict, year_output, year_style_dict,
             mileage_output, mileage_style_dict]
