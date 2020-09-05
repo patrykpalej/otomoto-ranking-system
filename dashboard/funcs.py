17eacb36d474a74d6902d634a7d31ff664d6ce10
Patryk Palej <patrykpalej@gmail.com>
2020-09-05 10:30:33 +0200

    Finalize function for hint texts logic

--- dashboard/funcs.py
+++ dashboard/funcs.py
@@ -3,31 +3,44 @@ from plotly.subplots import make_subplots
 
 
 def input_hint(hint_text, style_dict, input_1, input_2, min_limit, max_limit,
-               error_message, unit):
+               error_message, unit, thousand_sep):
 
-    if input_1 and input_2:
-        hint_text += input_1 + "{} - ".format(unit) + input_2 + unit
+    user_min_limit = min_limit
+    user_max_limit = max_limit
 
-        if int(input_1) < min_limit or int(input_2) > max_limit:
-            hint_text = "Wybierz z zakresu {}{} - {}{}"\
-                .format(str(int(min_limit)), unit, str(int(max_limit)), unit)
-            style_dict["color"] = "red"
+    # 1. Change limits based on user input
+    if input_1:
+        user_min_limit = int(input_1)
+    if input_2:
+        user_max_limit = int(input_2)
 
-        if int(input_1) > int(input_2):
+    # 2. Update hint text based on user input
+    if thousand_sep:
+        hint_text += str('{:,}'.format(user_min_limit).replace(",",  " ")) \
+                     + unit + " - " \
+                     + str('{:,}'.format(user_max_limit).replace(",",  " ")) \
+                     + unit
+    else:
+        hint_text += str(user_min_limit) + unit + " - " + str(user_max_limit) \
+            + unit
+
+    # 3. Handle errors
+    if (input_1 not in [None, ""]) or (input_2 not in [None, ""]):
+        # a) min > max
+        if user_min_limit > user_max_limit:
             hint_text = error_message
             style_dict["color"] = "red"
 
-    return hint_text
+        # b) limits exceeded
+        if user_min_limit < min_limit or user_max_limit > max_limit:
+            hint_text = "Wybierz z zakresu {}{} - {}{}"\
+                .format(min_limit, unit, max_limit, unit)
+            style_dict["color"] = "red"
 
+    # 4. Set user limit
+    if not (max_limit >= user_min_limit >= min_limit
+            and max_limit >= user_max_limit >= min_limit):
+        user_min_limit = min_limit
+        user_max_limit = max_limit
 
-def draw_plot(backend_input1):
-    plot = make_subplots()
-
-    if 1 in backend_input1:
-        plot.add_trace(go.Scatter(x=[1, 2], y=[3, 4]))
-
-    if 2 in backend_input1:
-        plot.add_trace(go.Scatter(x=[1, 2], y=[4, 3],
-                       marker=dict(color="orange")))
-
-    return plot
+    return hint_text, user_min_limit, user_max_limit
--- dashboard/logic.py
+++ dashboard/logic.py
@@ -4,60 +4,45 @@ from funcs import *
 def backend(price_input_1, price_input_2, year_input_1, year_input_2,
             mileage_input_1, mileage_input_2):
     # 1. Price
-    min_limit = 2000
-    max_limit = 80000
+    min_price = 2000
+    max_price = 80000
     price_output = "Cena: "
     price_style_dict = {"width": "15%", "position": "absolute", "left": "5%",
                         "top": "10%", "height": "10%", "font-size": 18,
                         "font-family": "Cambria"}
+    error_message = "Minimalna cena jest większa od maksymalnej"
 
-    if price_input_1:
-        min_limit = int(price_input_1)
-
-    if price_input_2:
-        max_limit = int(price_input_2)
-
-    price_output \
-        += str('{:,}'.format(min_limit).replace(",",  " ")) \
-        + "zł - " + str('{:,}'.format(max_limit).replace(",", " ")) \
-          + "zł"
-
-    if price_input_1 or price_input_2:
-        try:
-            if int(price_input_1) < 2000 or int(price_input_2) > 80000:
-                price_output = "Wybierz z zakresu 2 000zł - 80 000zł"
-                price_style_dict["color"] = "red"
-        except TypeError:
-            pass
-
-        try:
-            if int(price_input_1) > int(price_input_2):
-                price_output = "Cena minimalna jest większa od maksymalnej"
-                price_style_dict["color"] = "red"
-        except TypeError:
-            pass
+    price_output, price_min_limit, price_max_limit \
+        = input_hint(price_output, price_style_dict, price_input_1,
+                     price_input_2, min_price, max_price, error_message, "zł",
+                     1)
 
     # 2. Production year
+    min_year = 2000
+    max_year = 2019
     year_output = "Rok produkcji: "
     year_style_dict = {"width": "15%", "position": "absolute", "left": "20%",
                        "top": "10%", "height": "10%", "font-size": 18,
                        "font-family": "Cambria"}
     error_message = "Minimalny rok produkcji jest większy od maksymalnego"
 
-    year_output = input_hint(year_output, year_style_dict, year_input_1,
-                             year_input_2, 2000, 2019, error_message, "")
+    year_output, year_min_limit, year_max_limit \
+        = input_hint(year_output, year_style_dict, year_input_1,
+                     year_input_2, min_year, max_year, error_message, "", 0)
 
     # 3. Mileage
+    min_mil = int(1e4)
+    max_mil = int(1e6)
     mileage_output = "Przebieg: "
     mileage_style_dict = {"width": "15%", "position": "absolute",
-                          "left": "35%",
-                          "top": "10%", "height": "10%", "font-size": 18,
-                          "font-family": "Cambria"}
+                          "left": "35%", "top": "10%", "height": "10%",
+                          "font-size": 18, "font-family": "Cambria"}
     error_message = "Minimalny przebieg jest większy od maksymalnego"
 
-    mileage_output = input_hint(mileage_output, mileage_style_dict,
-                                mileage_input_1, mileage_input_2, 1e4, 1e6,
-                                error_message, "km")
+    mileage_output, mileage_min_limit, mileage_max_limit \
+        = input_hint(mileage_output, mileage_style_dict, mileage_input_1,
+                     mileage_input_2, min_mil, max_mil, error_message, "km",
+                     1)
 
     return [price_output, price_style_dict, year_output, year_style_dict,
             mileage_output, mileage_style_dict]
